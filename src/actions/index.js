7426981ea80b334d69fe5a783e26823c3fed7401
Anton <anton.forsberg89@gmail.com>
2017-09-20 21:03:51 +0200

    Start to implement middleware and fetching data with axios

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const API_KEY = '080ac0deb0d18e0c0474535f5b271b9d';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},se`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
